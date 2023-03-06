1ea2c3d3b751616f1e5c3e7a2f3e8339613c47d7
Junior <abelsouzacosta@gmail.com>
2023-03-06 16:07:33 -0300

    chore(movies): entity
    adding movie entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,21 @@
-export class Movie {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  collection: 'movies',
+  timestamps: true,
+})
+export class Movie {
+  @Prop({ required: true, type: String })
+  title: string;
+
+  @Prop({ required: true, type: String })
+  description: string;
+
+  @Prop({ required: true, type: String })
+  duration: string;
+
+  @Prop({ required: false, type: Number, default: 0 })
+  note: number;
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
